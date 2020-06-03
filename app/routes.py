00f1151fb95850d7a7d0e4bd00afa6fc13313f9b
Albert Rigo <albert.rigo@gmail.com>
2020-06-03 22:52:12 +0200

    more

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,46 @@
+from flask import session, render_template, redirect, url_for
 from app import app
+from authlib.integrations.flask_client import OAuth, OAuthError
+
+oauth = OAuth(app)
+oauth.register(
+    name = 'chpp',
+    api_base_url = 'https://api.twitter.com/1.1/',
+    request_token_url = app.config['REQUEST_TOKEN_PATH'],
+    access_token_url = app.config['ACCESS_TOKEN_PATH'],
+    authorize_url = app.config['AUTHORIZE_PATH'],
+    fetch_token=lambda: session.get('token'),  # DON'T DO IT IN PRODUCTION
+)
+
+@app.errorhandler(OAuthError)
+def handle_error(error):
+    return render_template('error.html', error=error)
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!" + app.config['REQUEST_TOKEN_PATH']
+  user = {'username': session.get('user')}
+  debug = {'print': 'oauth.chpp.authorize_redirect(request_token_url)'}
+  return render_template('main.html', title='Home', user=user, debug=debug)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.chpp.authorize_redirect(redirect_uri)
+
+@app.route('/auth')
+def auth():
+    token = oauth.chpp.authorize_access_token()
+    url = 'account/verify_credentials.json'
+    resp = oauth.chpp.get(url, params={'skip_status': True})
+    user = resp.json()
+    # DON'T DO IT IN PRODUCTION, SAVE INTO DB IN PRODUCTION
+    session['token'] = token
+    session['user'] = user
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('token', None)
+    session.pop('user', None)
+    return redirect('/')
--- run.py
+++ run.py
@@ -10,21 +10,10 @@ from authlib.integrations.flask_client import OAuth, OAuthError
 app = Flask(__name__)
 #bootstrap = Bootstrap(app)
 
-
 from app import app
 
 #app.secret_key = '!secret'
 #app.config.from_object('config')
 
-#oauth = OAuth(app)
-#oauth.register(
-#    name = 'chpp',
-#    api_base_url = 'https://api.twitter.com/1.1/',
-#    request_token_url = app.config['RequestTokenPath'],
-#    access_token_url = app.config['AccessTokenPath'],
-#    authorize_url = app.config['AuthorizePath'],
-#    fetch_token=lambda: session.get('token'),  # DON'T DO IT IN PRODUCTION
-#)
 
 # =========================================================================================================
-
