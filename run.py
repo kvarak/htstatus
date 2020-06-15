611c9f3c77d67250762b6baa28c540c319fa46b8
Albert Rigo <albert.rigo@gmail.com>
2020-06-15 16:13:27 +0200

    refactor

--- run.py
+++ run.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 
-import os
-#import json
-#import random
 import platform
+from flask import Flask
 
-from flask import Flask, request, render_template, session
-#from flask_bootstrap import Bootstrap
-
-from authlib.integrations.flask_client import OAuth, OAuthError
+# =========================================================================================================
 
 print("Python version " + platform.python_version())
 
 app = Flask(__name__)
-#bootstrap = Bootstrap(app)
 
 from app import app
-
-#app.secret_key = '!secret'
-#app.config.from_object('config')
-
-
-# =========================================================================================================
