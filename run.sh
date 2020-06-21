e784b7aeb25cc16c26587b1e667b57d906b171e5
Albert Rigo <albert.rigo@gmail.com>
2020-06-21 20:31:22 +0200

    allow dev server

--- run.sh
+++ run.sh
@@ -3,8 +3,16 @@
 ./changelog.sh
 
 ## FLASK_ENV=development turns on debug mode when flask starts
-export FLASK_ENV=development
+if [ "$1" = "5000" ]
+then
+  export FLASK_ENV=development
+fi
 
 # --------------------------------------------------------------------------------
 
-env LOCAL_RUN=true FLASK_RUN_PORT=5000 FLASK_RUN_HOST=0.0.0.0 FLASK_APP=run.py flask run
+if [ "$1" = "5000" ] || [ "$1" = "5050" ]
+then
+  env LOCAL_RUN=true FLASK_RUN_PORT=$1 FLASK_RUN_HOST=0.0.0.0 FLASK_APP=run.py flask run
+else
+  exit
+fi
