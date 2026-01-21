ed4d62e8c608348d59e2800e4f5a5c4442319644
Albert Rigo <albert.rigo@gmail.com>
2026-01-21 09:15:02 +0100

    refactor: improve blueprint error handling
    - Enhance auth.py OAuth flow with better error handling
    - Refactor login function with early returns
    - Add competition data fetching in matches.py
    - Improve stats route debugging output

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,10 +1,10 @@
 """Authentication routes blueprint for HT Status application."""
 
-from flask import Blueprint, make_response, render_template, request, session
+from flask import Blueprint, make_response, redirect, render_template, request, session
 from pychpp import CHPP
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.utils import create_page, debug_print, dprint
+from app.utils import create_page, dprint
 from models import User
 
 # Create Blueprint for authentication routes
@@ -29,143 +29,165 @@ def setup_auth_blueprint(app_instance, db_instance, ck, cs):
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     """Handle user login and Hattrick OAuth authentication."""
-    # this comes from form
-    username = request.form.get('username')
-    password = request.form.get('password')
-    # this comes from CHPP
-    oauth_verifier = request.values.get('oauth_verifier')
-    oa = oauth_verifier
+    # Return early if already logged in
+    if session.get('current_user'):
+        return redirect('/')
 
-    if not (oa) and not (username) and session.get('current_user') is None:
+    # Handle OAuth callback
+    oauth_verifier = request.args.get('oauth_verifier')
+    if oauth_verifier:
+        return handle_oauth_callback(oauth_verifier)
+
+    # Handle GET request - show login form
+    if request.method == 'GET':
         return create_page(
             template='login.html',
             title='Login / Signup')
 
-    # Initialize CHPP instance
-    chpp = CHPP(consumer_key, consumer_secret)
+    # Handle POST request (form submission)
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    # if this returns a user, then the user already exists in database
-    user = db.session.query(User).filter_by(username=username).first()
+    # Validate form data
+    if not username:
+        return create_page(
+            template='login.html',
+            title='Login / Signup',
+            error='Username is required')
 
-    if username and user:
-        if check_password_hash(user.password, password):
-            dprint(1, "Login success")
-            # get stuff and add in session
-            session['access_key'] = user.access_key
-            session['access_secret'] = user.access_secret
-            session['current_user'] = user.ht_user
-            session['current_user_id'] = user.ht_id
+    if not password:
+        return create_page(
+            template='login.html',
+            title='Login / Signup',
+            error='Password is required')
+
+    if len(password) < 8:
+        return create_page(
+            template='login.html',
+            title='Login / Signup',
+            error='Password must be at least 8 characters long')
+
+    # Check for existing user
+    existing_user = User.query.filter_by(username=username).first()
+
+    if existing_user and check_password_hash(existing_user.password, password):
+        # Existing user login
+        if existing_user.access_key and existing_user.access_secret:
+            # User has OAuth tokens - log them in directly
+            session['access_key'] = existing_user.access_key
+            session['access_secret'] = existing_user.access_secret
+            session['current_user'] = existing_user.ht_user
+            session['current_user_id'] = existing_user.ht_id
+
+            # Setup team data
+            try:
+                chpp = CHPP(consumer_key, consumer_secret,
+                           session['access_key'], session['access_secret'])
+                current_user = chpp.user()
+                all_teams = current_user._teams_ht_id
+                all_team_names = []
+                for id in all_teams:
+                    all_team_names.append(chpp.team(ht_id=id).name)
+                session['all_teams'] = all_teams
+                session['all_team_names'] = all_team_names
+                session['team_id'] = all_teams[0]
+            except Exception as e:
+                dprint(1, f"Error setting up team data: {e}")
+
+            return redirect('/')
         else:
-            error = "Login failed"
-            return create_page(
-                template='login.html',
-                title='Login / Signup',
-                error=error)
-
-    else:
-        if not (oauth_verifier):
-            # New user, connect to CHPP to be able to create user
-            dprint(1, "New user, connect to CHPP to be able to create user")
-
-            if len(password) < 8:
-                # Password too short
-                error = "Password too short"
-                return create_page(
-                    template='login.html',
-                    title='Login / Signup',
-                    error=error)
-
-            auth = chpp.get_auth(callback_url=app.config['CALLBACK_URL'],
-                                 scope="")
-            # debug_print("login", "chpp.get_auth", auth._SOURCE_FILE)
-            session['request_token'] = auth["request_token"]
-            session['req_secret'] = auth["request_token_secret"]
+            # Need to get OAuth tokens for existing user
             session['username'] = username
-            session['password'] = generate_password_hash(password,
-                                                         method='sha256')
-            return render_template(
-                '_forward.html',
-                url=auth['url'])
+            session['password'] = generate_password_hash(password, method='sha256')
+            return start_oauth_flow()
 
+    # New user registration or invalid login
+    if existing_user:
+        # Wrong password
+        return create_page(
+            template='login.html',
+            title='Login / Signup',
+            error='Invalid username or password')
+
+    # New user - start OAuth flow
+    session['username'] = username
+    session['password'] = generate_password_hash(password, method='sha256')
+    return start_oauth_flow()
+
+
+def start_oauth_flow():
+    """Start OAuth flow with Hattrick."""
+    chpp = CHPP(consumer_key, consumer_secret)
+    auth = chpp.get_auth(callback_url=app.config['CALLBACK_URL'], scope="")
+
+    session['request_token'] = auth["request_token"]
+    session['req_secret'] = auth["request_token_secret"]
+
+    return render_template('_forward.html', url=auth['url'])
+
+
+def handle_oauth_callback(oauth_verifier):
+    """Handle OAuth callback after authorization."""
+    try:
+        # Get access tokens
+        chpp = CHPP(consumer_key, consumer_secret)
+        access_token = chpp.get_access_token(
+            request_token=session['request_token'],
+            request_token_secret=session['req_secret'],
+            code=oauth_verifier)
+
+        session['access_key'] = access_token['key']
+        session['access_secret'] = access_token['secret']
+
+        # Get user from Hattrick
+        chpp = CHPP(consumer_key, consumer_secret,
+                   session['access_key'], session['access_secret'])
+        current_user = chpp.user()
+
+        session['current_user'] = current_user.username
+        session['current_user_id'] = current_user.ht_id
+
+        # Check if user exists in database
+        existing_user = User.query.filter_by(ht_id=current_user.ht_id).first()
+
+        if existing_user:
+            # Update existing user with new tokens
+            User.claimUser(
+                existing_user,
+                username=session.get('username', current_user.username),
+                password=session.get('password', ''),
+                access_key=session['access_key'],
+                access_secret=session['access_secret'])
         else:
-            # New access permissions from Hattrick
-            dprint(1, "New access permissions from Hattrick")
-            access_token = chpp.get_access_token(
-                           request_token=session['request_token'],
-                           request_token_secret=session['req_secret'],
-                           code=oauth_verifier)
+            # Create new user
+            new_user = User(
+                ht_id=current_user.ht_id,
+                ht_user=current_user.username,
+                username=session.get('username', current_user.username),
+                password=session.get('password', ''),
+                access_key=access_token['key'],
+                access_secret=access_token['secret'])
+            db.session.add(new_user)
 
-            # debug_print(
-            #     "login",
-            #     "chpp.get_access_token",
-            #     access_token._SOURCE_FILE)
+        db.session.commit()
 
-            session['access_key'] = access_token['key']
-            session['access_secret'] = access_token['secret']
+        # Setup team data
+        all_teams = current_user._teams_ht_id
+        all_team_names = []
+        for id in all_teams:
+            all_team_names.append(chpp.team(ht_id=id).name)
+        session['all_teams'] = all_teams
+        session['all_team_names'] = all_team_names
+        session['team_id'] = all_teams[0]
 
-            chpp = CHPP(consumer_key,
-                        consumer_secret,
-                        session['access_key'],
-                        session['access_secret'])
+        return redirect('/')
 
-            current_user = chpp.user()
-            debug_print("login", "chpp.user", current_user._SOURCE_FILE)
-            session['current_user'] = current_user.username
-            session['current_user_id'] = current_user.ht_id
-
-            # check if the user exists already in the database
-            ht_id = (db.session.query(User)
-                     .filter_by(ht_id=current_user.ht_id)
-                     .first())
-
-            if ht_id:
-                # existing ht_id in db
-                # then reassign the ownership
-                dprint(1, "existing ht_id in db")
-                User.claimUser(
-                    ht_id,
-                    username=session['username'],
-                    password=session['password'],
-                    access_key=session['access_key'],
-                    access_secret=session['access_secret'])
-                db.session.commit()
-
-            else:
-                # create new user with the form data.
-                dprint(1, "create new user")
-                new_user = User(
-                    ht_id=current_user.ht_id,
-                    ht_user=current_user.username,
-                    username=session['username'],
-                    password=session['password'],
-                    access_key=access_token['key'],
-                    access_secret=access_token['secret'])
-                # add the new user to the database
-                db.session.add(new_user)
-                db.session.commit()
-
-    dprint(1, "UserID: ", session['current_user_id'])
-
-    chpp = CHPP(consumer_key,
-                consumer_secret,
-                session['access_key'],
-                session['access_secret'])
-
-    current_user = chpp.user()
-    debug_print("login", "chpp.user", current_user._SOURCE_FILE)
-    all_teams = current_user._teams_ht_id
-    all_team_names = []
-    for id in all_teams:
-        all_team_names.append(chpp.team(ht_id=id).name)
-    session['all_teams'] = all_teams
-    session['all_team_names'] = all_team_names
-
-    session['team_id'] = all_teams[0]
-
-    return create_page(
-        template='main.html',
-        title='Logged in')
-
+    except Exception as e:
+        dprint(1, f"OAuth callback error: {e}")
+        return create_page(
+            template='login.html',
+            title='Login / Signup',
+            error='OAuth authentication failed. Please try again.')
 
 @auth_bp.route('/logout')
 def logout():
--- app/blueprints/matches.py
+++ app/blueprints/matches.py
@@ -77,6 +77,7 @@ def matches():
 @matches_bp.route('/stats')
 def stats():
     """Display team statistics."""
+
     if session.get('current_user') is None:
         return render_template(
             '_forward.html',
@@ -117,7 +118,9 @@ def stats():
         get_top_performers,
         get_top_scorers,
     )
+    print(f"\n=== STATS ROUTE DEBUG: About to call calculate_team_statistics with {len(current_players_list)} players ===")
     team_stats = calculate_team_statistics(current_players_list)
+    print(f"=== STATS ROUTE DEBUG: Got team_stats result: {team_stats} ===\n")
 
     # Get top performers
     top_scorers = get_top_scorers(current_players_list)
